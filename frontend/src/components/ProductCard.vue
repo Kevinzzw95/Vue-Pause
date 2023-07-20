f04327dfea6d2d49ab7cda76aa061fdf6414bf48
Zhiwen Zhang <z2294zha@uwaterloo.ca>
2023-07-19 22:19:51 -0400

    V0719

--- frontend/src/components/ProductCard.vue
+++ frontend/src/components/ProductCard.vue
@@ -1,35 +1,33 @@
 <template>
     <div>
         <div class="relative flex card card-compact shadow-lg max-w-[18rem] bg-base object-cover">
-            <button @click="showDetails()" class="self-center w-full">
+            <label for="my_modal" @click="showDetails()" class="self-center w-full">
                 <figure><img :src=item.imageUrl[0] alt="Puzzle" class="object-cover w-full h-[13rem] md:h-[18rem] max-h-[20rem]"/></figure>
                 <!-- <img v-if="!item.active" src="../assets/coming-soon.png" class="absolute top-[25%] md:left-10 w-[12rem]" /> -->
                 <!-- <img v-if="item.unitsInStock === 0" src="../assets/sold-out.png" class="absolute top-[25%] md:left-10 w-[12rem]" /> -->
-            </button>
+            </label>
             <div v-if="item.special" class="absolute box rounded-r-md w-[5rem] h-8 top-3 bg-gradient-to-r from-green-400 to-blue-500 shadow-xl flex items-stretch p-2">
                 <h1 className="text-white text-xs self-center">{{ item.special }}</h1>
             </div>
             <div class="grid grid-rows-4 h-[4.5rem] md:h-[5rem] p-1 font-primary">
                 <!-- <router-link :to="'/details/' + item.id" class="self-center"> -->
-                <button @click="showDetails()" class="self-start items-center row-span-3">
+                <label for="my_modal" @click="showDetails()" class="self-start items-center row-span-3">
                     <h2 class="text-xs md:text-md flex flex-col font-bold">
                         <p>{{ item.name.split(" ")[0] }}</p>
                         <p>{{ item.name.split(/ (.*)/)[1] }}</p>
                     <!-- <div class="badge badge-secondary">NEW</div> -->
                     </h2>
-                </button>
+                </label>
                     
-                
+                <input type="checkbox" id="my_modal" class="modal-toggle" />
                 <!-- </router-link> -->
-                <dialog ref="detail_modal" class="modal modal-bottom sm:modal-middle">
-                    <form method="dialog" class="modal-box relative min-w-[90%] h-[75%]">
+                <div class="modal modal-bottom sm:modal-middle">
+                    <div v-if="isOpen" class="modal-box relative min-w-[90%] h-[75%]">
                         <product-details :item="item" :key="item.sku"/>
                         
-                    </form>
-                    <form v-if="isOpen" method="dialog" class="modal-backdrop">
-                        <button>close</button>
-                    </form>
-                </dialog>
+                    </div>
+                    <label class="modal-backdrop" for="my_modal">Close</label>
+                </div>
 			
                 <div class="flex flex-row justify-end font-default text-xs space-x-1">
                     <div v-if="item.unitsInStock === 0" class="inline-flex badge badge-outline badge-sm md:badge-md text-deep">Sold Out</div>
@@ -64,6 +62,6 @@ onBeforeUpdate(() => {
 
 const showDetails = () => {
     isOpen.value = true;
-    detail_modal.value!.showModal();
+    //detail_modal.value!.showModal();
 }
 </script>
